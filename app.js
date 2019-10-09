2d6d74d2dd2893d07c188cab9e636578316d08a6
Bartosz Dobrosielski <dobry1@pm.me>
2019-10-09 12:53:18 +0000

    add decripton to db and show.ejs

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 var nianiaSchema = new mongoose.Schema({
 	name: String,
 	image: String,
+	desc: String //description of niania
 });
 
 var Niania = mongoose.model("Niania", nianiaSchema);
 
 // Niania.create(
 // 	{name: "Greta",
-// 	 image: "https://polki.pl/foto/1_X_LARGE/najpiekniejsze-twarze-swiata-109848.webp"
+// 	 image: "https://polki.pl/foto/1_X_LARGE/najpiekniejsze-twarze-swiata-109848.webp",
+// 	 desc: "Bacon ipsum dolor amet pastrami tri-tip short loin ground round tongue ham bresaola spare ribs hamburger kielbasa filet mignon buffalo frankfurter turkey tenderloin. Meatloaf jowl boudin,  shankle drumstick pork chop 	                     andouille tbone ham hock sirloin filet mignon pork belly Brisket filet mignon cow beef hamburger. Swine meatball prosciutto ground round frankfurter flank cupim. Tail salami hamburger fatback capicola"
 // 	}, function (err, niania){
 // 		if(err){
 // 			console.log("Error: "+err);
@@ -62,7 +64,18 @@ app.post("/niania", function(req, res){
 
 app.get("/niania/new", function(req, res){
 	res.render("new");
-})
+});
+//Show description about nianias
+app.get("/niania/:id", function(req, res){
+	var idParams =  req.params.id;
+	Niania.findById(idParams, function(err, foundNiania){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("show", {niania: foundNiania});
+		}						 
+	});
+});
 
 app.listen(3000, function(){
 	console.log("+++++++++++++++UP AND RUNNING++++++++++++++++");
